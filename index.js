f193e12c865eb40d719c1d09ea11b174e5fdff91
Sivasubramanyam A <ssdragonar5@gmail.com>
2019-03-11 21:37:53 +0530

    Fix included hook to make this work with engines

--- index.js
+++ index.js
@@ -13,10 +13,15 @@ module.exports = {
     }
   },
 
-  included: function colpick_included() {
+  included: function() {
     this._super.included.apply(this, arguments);
 
+    let findHost = this._findHost;
+    let app = findHost.call(this);
+
+    this.app = app;
+
     let pickr = path.join('node_modules', 'pickr-widget', 'dist');
-    this.app.import(path.join(pickr, 'pickr.min.css'));
+    app.import(path.join(pickr, 'pickr.min.css'));
   }
 };
