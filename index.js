a3146174d2d43c9a8cf579fd48d9514ae3f65364
Sivasubramanyam A <ssdragonar5@gmail.com>
2019-03-16 13:45:30 +0530

    Choose bundle based on the project's targets

--- index.js
+++ index.js
@@ -1,16 +1,25 @@
 'use strict';
 
 const path = require('path');
+const caniuse = require('caniuse-api')
 
 module.exports = {
   name: require('./package').name,
 
   options: {
     autoImport: {
-      alias: {
-        pickr: '@simonwep/pickr/dist/pickr.min.js'
-      }
+      alias: {}
     }
+  },
+
+  choosePickrForTargets(targets = {}) {
+    let { browsers = [] } = targets;
+    let browserQuery = browsers.join(',');
+    if (caniuse.isSupported('es6-module', browserQuery)) {
+      return '@simonwep/pickr/dist/pickr.min.js';
+    }
+
+    return '@simonwep/pickr/dist/pickr.es5.min.js';
   },
 
   included: function() {
@@ -23,5 +32,8 @@ module.exports = {
 
     let pickr = path.join('node_modules', '@simonwep/pickr', 'dist');
     app.import(path.join(pickr, 'pickr.min.css'));
+
+    let targets = this.project.targets;
+    this.options.autoImport.alias.pickr = this.choosePickrForTargets(targets);
   }
 };
